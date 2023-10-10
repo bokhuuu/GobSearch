09ea5ba864d76f1a33b361a8353fdf5e72de19cf
bokhuuu <zurabokhua@gmail.com>
2023-10-10 18:56:12 +0400

    nested routes and layouts

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,19 @@ import {
 import Home from "./pages/Home";
 import About from "./pages/About";
 import RootLayout from "./layout/RootLayout";
+import HelpLayout from "./layout/HelpLayout";
+import FAQ from "./pages/help/FAQ";
+import Contact from "./pages/help/Contact";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="help" element={<HelpLayout />}>
+        <Route path="faq" element={<FAQ />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
     </Route>
   ))
 
