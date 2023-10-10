35123dc737adedcbb0e6ecff65d96ec10f39f8c1
bokhuuu <zurabokhua@gmail.com>
2023-10-10 17:28:36 +0400

    router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import RootLayout from "./layout/RootLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Route>
+  ))
+
 function App() {
   return (
-    <div className="App">
-      Hello, Ninjas!
-    </div>
+    <RouterProvider router={router} />
   );
 }
-
 export default App
